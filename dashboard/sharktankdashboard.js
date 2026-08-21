b55690fda9f962d48fa88b3f1a31ddb6018fb96a
PolonBear <polon@polonbear.com>
2026-08-21 12:21:19 +0200

    added comments

--- dashboard/sharktankdashboard.js
+++ dashboard/sharktankdashboard.js
@@ -1,3 +1,9 @@
+/*
+JS for the Shark Tank Dashboard custom Tab, used for integrating non-standard functionality. This is a custom tab that is not part of the standard overlay controls.
+If you want a custom feature for this is the place to add it.
+*/
+
+// Start of the Pibble Mode Toggle
 const PibbleModeEnabled = nodecg.Replicant('PibbleModeEnabled', {defaultValue: false});
 const PibbleModeToggle = document.getElementById('pibble-mode-toggle');
 
@@ -8,3 +14,4 @@ PibbleModeToggle.addEventListener('change', e => {
 PibbleModeEnabled.on('change', newValue => {
     PibbleModeToggle.checked = newValue;
 });
+// End of the Pibble Mode Toggle
